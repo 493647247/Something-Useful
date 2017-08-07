a6c3c1d41ea2b5dbb4ced85e610f7ef1e1fcc456
chenjunyu19 <1335546814@qq.com>
2017-08-08 00:03:17 +0800

    优化错误提示

--- QZone-Cleaner.js
+++ QZone-Cleaner.js
@@ -8,19 +8,23 @@ function clickDelete() {
     document.querySelector('.app_canvas_frame').contentDocument.querySelector('.del_btn').click();
     setTimeout('clickYes();', CleanerDelay);
   } catch (error) {
-    console.info('预期内的错误', error);
-    document.querySelector('.app_canvas_frame').contentDocument.querySelector('#pager_next_0').click();
-    console.info('已尝试点击 下一页，若无反应请手动点击')
+    console.info('预期内的错误：尝试点击删除按钮时出错，将尝试自动翻页。', error);
+    try {
+      document.querySelector('.app_canvas_frame').contentDocument.querySelector('#pager_next_0').click();
+      console.info('已尝试自动翻页，若无反应请手动翻页。');
+    } catch (error) {
+      console.error('预期外的错误：自动翻页失败。', error);
+    }
   }
 }
 
 function clickYes() {
   try {
     document.querySelector('.qz_dialog_layer_sub').click();
-    console.info('已删除', cleanerCount++, '条说说');
+    console.info('已删除', cleanerCount++, '条说说。');
     setTimeout('clickDelete();', CleanerDelay);
   } catch (error) {
-    console.error('预期外的错误', error);
+    console.error('预期外的错误。', error);
   }
 }
 
